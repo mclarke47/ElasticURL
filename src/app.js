538c200e594e466b8b2e9f5d38c36665789edb46
mclarke47 <matthewclarke47@hotmail.co.uk>
2015-03-03 23:09:30 +0000

    simplified screen

--- src/app.js
+++ src/app.js
@@ -5,55 +5,55 @@ var EsStatus = React.createClass({
         var elasticsearchBaseUrl = "http://localhost:9200";
         console.log("getEsStatus");
         var esStatus = this;
-        $.get( elasticsearchBaseUrl)
-            .done(function( data ) {
+        $.get(elasticsearchBaseUrl)
+            .done(function (data) {
                 console.log("done");
                 console.log(data);
                 var newState = esStatus.mapToState(data);
                 esStatus.setState(newState);
-            }).fail(function() {
+            }).fail(function () {
                 console.log("fail");
             });
     },
     mapToState: function (esStatus) {
-        return( {
-            "status" : esStatus.status,
-            "name" : esStatus.name,
-            "cluster_name" : esStatus.cluster_name,
-            "version" : {
-                "number" : esStatus.version.number,
-                "build_hash" : esStatus.version.build_hash,
-                "build_timestamp" : esStatus.version.build_timestamp,
-                "build_snapshot" : esStatus.version.build_snapshot,
-                "lucene_version" : esStatus.version.lucene_version
+        return ( {
+            "status": esStatus.status,
+            "name": esStatus.name,
+            "cluster_name": esStatus.cluster_name,
+            "version": {
+                "number": esStatus.version.number,
+                "build_hash": esStatus.version.build_hash,
+                "build_timestamp": esStatus.version.build_timestamp,
+                "build_snapshot": esStatus.version.build_snapshot,
+                "lucene_version": esStatus.version.lucene_version
             },
-            "tagline" : esStatus.tagline
+            "tagline": esStatus.tagline
         });
     },
-    refresh: function(){
+    refresh: function () {
 
         this.getEsStatus();
 
     },
-    getInitialState: function(){
+    getInitialState: function () {
         return {
-            "status" : 0,
-            "name" : "unknown",
-            "cluster_name" : "unknown",
-            "version" : {
-                "number" : "unknown",
-                "build_hash" : "unknown",
-                "build_timestamp" : "unknown",
-                "build_snapshot" : false,
-                "lucene_version" : "unknown"
+            "status": 0,
+            "name": "unknown",
+            "cluster_name": "unknown",
+            "version": {
+                "number": "unknown",
+                "build_hash": "unknown",
+                "build_timestamp": "unknown",
+                "build_snapshot": false,
+                "lucene_version": "unknown"
             },
-            "tagline" : "unknown"
+            "tagline": "unknown"
         }
     },
-    render : function(){
+    render: function () {
 
         var divStyle = {};
-        if(this.state.status===200){
+        if (this.state.status === 200) {
             divStyle.backgroundColor = "lightgreen";
         }
 
@@ -80,28 +80,32 @@ var EsCluster = React.createClass({
         var elasticsearchBaseUrl = "http://localhost:9200";
         console.log("getEsStatus");
         var esCluster = this;
-        $.get( elasticsearchBaseUrl+"/_nodes")
-            .done(function( data ) {
+        $.get(elasticsearchBaseUrl + "/_nodes")
+            .done(function (data) {
                 console.log("done");
                 console.log(data);
                 esCluster.setState(data);
-            }).fail(function() {
+            }).fail(function () {
                 console.log("fail");
             });
     },
-    refresh: function(){
+    refresh: function () {
         this.getEsClusterStatus();
 
     },
-    getInitialState: function(){
-        return({});
+    getInitialState: function () {
+        this.getEsClusterStatus();
+        return ({});
     },
-    render : function(){
+    render: function () {
         var nodes = {};
         var scope = this;
-        if(this.state.nodes){
-            Object.keys(this.state.nodes).forEach(function(entry){
-                nodes["node-"+entry] = scope.state.nodes[entry];
+        if (this.state.nodes) {
+            Object.keys(this.state.nodes).forEach(function (entry) {
+                console.log(scope.state.nodes[entry]);
+                nodes["node-" + entry] = <li>
+                    <Node nodeData={scope.state.nodes[entry]}></Node>
+                </li>;
             });
         }
 
@@ -109,22 +113,35 @@ var EsCluster = React.createClass({
         return (
             <div className="clusterStatus">
                 <h1>Cluster: {this.state.cluster_name}</h1>
-                <ol>
+                <ul>
                     {nodes}
-                </ol>
+                </ul>
                 <button type="button" onClick={this.refresh}>refresh</button>
             </div>
         );
     }
 });
 
-var node = React.createClass({
+var Node = React.createClass({
 
+    getInitialState: function () {
+        return ({
+            name: this.props.nodeData.name,
+            host: this.props.nodeData.host
+        });
+    },
+    render: function () {
 
-    render: function(){
-        return ;
+        console.log(this);
+
+        return (
+            <div className="nodeContainer">
+                <h2>Node: {this.state.name}</h2>
+                <h2>Host: {this.state.host}</h2>
+            </div>
+        );
     }
 });
 
-React.renderComponent(<EsStatus/>, document.getElementById('mount'));
+//React.renderComponent(<EsStatus/>, document.getElementById('mount'));
 React.renderComponent(<EsCluster/>, document.getElementById('drilldown-mount'));
